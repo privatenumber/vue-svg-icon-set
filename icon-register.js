1882527a39bdffced517ed936da074d51bc0b362
Hiroki Osame <hiroki.osame@gmail.com>
2020-02-24 14:10:57 -0500

    Support custom component

--- icon-register.js
+++ icon-register.js
@@ -8,12 +8,17 @@ export default {
 			type: String,
 			required: true,
 		},
+		el: {
+			type: null,
+			default: 'svg',
+		},
 	},
 	inject: {
 		svgLayer,
 	},
 	render(h, ctx) {
+		// TODO: Does registering this here have negative SSR implications?
 		const id = ctx.injections.svgLayer.register(ctx.props.id, ctx.children[0]);
-		return h('svg', [h('use', { attrs: { 'xlink:href': `#${id}` }})]);
+		return h(ctx.props.el, [h('use', { attrs: { 'xlink:href': `#${id}` }})]);
 	},
 };
